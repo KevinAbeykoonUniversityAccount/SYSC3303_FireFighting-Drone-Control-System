44c73e6ebbd3189cda55ebc1fe5d03e0014dd599
abkhan0517 <abkhan0517@gmail.com>
2026-04-05 21:09:01 -0400

    Added EventLog class to display logs properly.

--- src/EventLogger.java
+++ src/EventLogger.java
@@ -1,8 +1,48 @@
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.*;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
 public class EventLogger implements Runnable{
+    private class EventLog {
+        final long time;
+        final String entity;
+        final String code;
+        final String[] data;
+
+        public EventLog(long time, String entity, String code, String... data) {
+            this.time = time;
+            this.entity = entity;
+            this.code = code;
+            this.data = data;
+        }
+
+        public String getEntity() {
+            return entity;
+        }
+
+        public String getCode() {
+            return code;
+        }
+
+        public String toString() {
+            Instant instantTime = Instant.ofEpochMilli(time);
+            String formattedTime = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS").withZone(ZoneId.systemDefault()).format(instantTime);
+
+            String logString = "Event log: [" + formattedTime + ", " + entity + ", " + code;
+
+            for (String thing : data) {
+                logString += ", " + thing;
+            }
+
+            logString += "]";
+
+            return  logString;
+        }
+    }
+
     public final static int DEFAULT_PORT = 9000;
 
     private DatagramSocket reciever;
@@ -17,17 +57,20 @@ public class EventLogger implements Runnable{
         this.reciever = new DatagramSocket(port);
     }
 
-    public String recieve() {
+    public EventLog recieve() {
         byte[] buffer = new byte[100];
+        EventLog log;
         try {
             DatagramPacket event = new DatagramPacket(buffer, buffer.length);
             reciever.receive(event);
-            String eventStr = new String(event.getData(), 0, event.getLength());
-            return eventStr;
+            String[] eventStr = new String(event.getData(), 0, event.getLength()).split(",");
+            if (eventStr.length == 2) log = new EventLog(System.currentTimeMillis(), eventStr[0], eventStr[1]);
+            else log = new EventLog(System.currentTimeMillis(), eventStr[0], eventStr[1], eventStr[2]);
+            return log;
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return "Log Error";
+        return null;
     }
 
     public void writeLog(String event) {
@@ -49,10 +92,10 @@ public class EventLogger implements Runnable{
         }
     }
 
-    public void checkFlags(String event) {
-        if (false) schedulerRunning = false;
-        if (false) fireSystemRunning = false;
-        if (false) droneSystemRunning = false;
+    public void checkFlags(EventLog event) {
+        if (event.getEntity().equals("Scheduler") && event.code.equals("ENDED")) schedulerRunning = false;
+        if (event.getEntity().equals("FireSubsystem") && event.code.equals("ENDED")) fireSystemRunning = false;
+        if (event.getEntity().equals("DroneSubsystem") && event.code.equals("ENDED")) droneSystemRunning = false;
     }
 
     public void displayMetrics() {
@@ -61,10 +104,11 @@ public class EventLogger implements Runnable{
 
     @Override
     public void run() {
+        clearLogFile();
         System.out.println("Starting Event Logger");
         while (schedulerRunning | fireSystemRunning | droneSystemRunning) {
-            String event = recieve();
-            writeLog(event);
+            EventLog event = recieve();
+            writeLog(event.toString());
             checkFlags(event);
         }
         displayMetrics();
